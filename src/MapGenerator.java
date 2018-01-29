7d6539811cf476c3bbcad2c4eb2ed943b6990635
Patrick-Raphael Perez <patrickraphaelperez@gmail.com>
2018-01-29 15:27:56 -0800

    finished MapGenerator class
    added strokes for bricks

--- src/MapGenerator.java
+++ src/MapGenerator.java
@@ -1,3 +1,4 @@
+import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Graphics2D;
 
@@ -23,8 +24,16 @@ public class MapGenerator {
 				if(map[i][j] > 0) {
 					g.setColor(Color.white);
 					g.fillRect(j * brickWidth + 80, i * brickHeight+ 50, brickWidth, brickHeight);
+					
+					g.setStroke(new BasicStroke(3));
+					g.setColor(Color.black);
+					g.drawRect(j * brickWidth + 80, i * brickHeight+ 50, brickWidth, brickHeight);
 				}
 			}
 		}		
+	}
+	
+	public void setBrickValue(int value, int row, int column){
+		map[row][column] = value;
 	}
 }
